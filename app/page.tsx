388aad52b81d3ca678c471541f6eaa0a2012c671
gipo-ski <koladiji@gmail.com>
2024-01-24 09:40:48 +0000

    added dbConfig and models folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+// import Image from "next/image";
 import { useState } from "react";
+import { FaTrashCan, FaPenToSquare } from "react-icons/fa6";
 
 export default function Todos() {
   const [inputText, setInputText] = useState("");
@@ -9,7 +11,7 @@ export default function Todos() {
   return (
     <main className="flex flex-col gap-8 items-center justify-between p-24">
       <div className="flex flex-col gap-2 bg-slate-400 rounded-md p-5">
-        <div>
+        <div className="flex flex-col gap-8 pb-8 items-center">
           <h1 className="text-3xl">My Todo List</h1>
         </div>
         <div className="flex gap-2 mb-7">
@@ -25,7 +27,7 @@ export default function Todos() {
           </button>
           <button className="bg-red-500 hover:bg-red-600 rounded-md py-1 px-3">
             Clear Todo
-          </button> 
+          </button>
         </div>
         <div className="w-5/6 flex flex-col gap-2">
           {todos.map((todo, index) => {
@@ -35,14 +37,16 @@ export default function Todos() {
                 className="bg-slate-600 flex gap-1 justify-between items-center p-2 rounded-md shadow-md"
               >
                 <div className="flex gap-2">
-                  <input type="checkbox" />
+                  <input type="checkbox" className="w-5"/>
                   <p className="text-lg text-white">Write some code</p>
                 </div>
                 <div className="flex gap-2">
-                  <button className="bg-green-500 hover:bg-green-600 rounded-md shadow-md p-1">
+                  <button className="bg-green-500 hover:bg-green-600 rounded-md shadow-md p-1 flex gap-1">
+                    <FaPenToSquare className="text-xl pt-1" />
                     Edit
                   </button>
-                  <button className="bg-red-500 hover:bg-red-600 rounded-md shadow-md p-1">
+                  <button className="bg-red-500 hover:bg-red-600 rounded-md shadow-md p-1 flex gap-1">
+                    <FaTrashCan className="text-xl pt-1" />
                     Delete
                   </button>
                 </div>
